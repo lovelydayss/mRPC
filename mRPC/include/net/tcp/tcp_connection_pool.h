ee6e7a86971454c90dcd2318977890742a25a7f3
lovelydayss <137208403@qq.com>
2024-03-19 22:58:23 +0800

    add TCP connection pool use LRU Algorithm

---
+++ mRPC/include/net/tcp/tcp_connection_pool.h
@@ -0,0 +1,43 @@
+#ifndef MRPC_NET_TCP_TCP_CONNECTION_POOL_H
+#define MRPC_NET_TCP_TCP_CONNECTION_POOL_H
+
+
+#include "tcp_connection.h"
+#include "utils.h"
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+
+
+MRPC_NAMESPACE_BEGIN
+
+
+class TcpConnectionPool {
+
+public:
+    using s_ptr = std::shared_ptr<TcpConnectionPool>;
+    using u_ptr = std::unique_ptr<TcpConnectionPool>;
+
+
+public:
+    static const TcpConnectionPool::s_ptr& GetGlobalTcpConnectionPool();
+
+
+private:
+    std::vector<TcpConnection::s_ptr> m_tcp_connections;
+    std::unordered_map<TcpConnection::s_ptr, int> m_connection_map;
+
+};
+
+
+
+
+
+
+
+MRPC_NAMESPACE_END
+
+
+
+#endif
---
+++ mRPC/src/net/tcp/tcp_connection_pool.cc
@@ -0,0 +1,21 @@
+#include "tcp_connection_pool.h"
+#include "utils.h"
+#include <cstddef>
+#include <mutex>
+
+MRPC_NAMESPACE_BEGIN
+
+static TcpConnectionPool::s_ptr s_ptr_connection_pool = nullptr;
+static std::once_flag singleton_flag;
+
+const TcpConnectionPool::s_ptr& TcpConnectionPool::GetGlobalTcpConnectionPool() {
+    
+    std::call_once(singleton_flag, [](){
+        
+    });
+
+    return s_ptr_connection_pool;
+}
+
+
+MRPC_NAMESPACE_END
